61f71a18df6fc65006c2f8cc85cc6ee325c6146d
andriashengeli <andriashengelia5@gmail.com>
2024-06-27 20:37:57 +0400

    first commit

---
+++ day 14/classwork/main.py
@@ -0,0 +1,5 @@
+name=input("enter your name: ")
+
+
+last_name=input("enter your name: ")
+print(name + " " + last_name)
---
+++ day 14/homework/main.py
@@ -0,0 +1,25 @@
+name=input("enter your name: ")
+print=(name)
+print(type(name)) 
+
+# ინახავს ათ წილადს 
+
+height=float(input("enter your height: "))
+print(height)
+print(type(height))
+
+# ინახავს ათწილადს 
+last_name = float(("enter your last_name: "))
+print(last_name)
+print(type(last_name))
+
+num1=20
+num2=float(num1)
+print(num2)
+print(type(num2))
+
+
+
+
+
+
