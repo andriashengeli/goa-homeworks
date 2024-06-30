8152b844a7ffcc9a2d3e31c6391a902ebbb32efe
andriashengeli <andriashengelia5@gmail.com>
2024-06-30 13:09:54 +0400

    second commit

---
+++ day 08/classwork/main.py
@@ -0,0 +1,7 @@
+# making variables
+name=("andria")
+age=(13)
+
+#maiking integer
+level=5
+goal=10
---
+++ day 10/classwork/main.py
@@ -0,0 +1,5 @@
+# about my self 
+my_name=("andria")
+my_surname=("shengelia")
+print("my_name")
+print("my_surname")
